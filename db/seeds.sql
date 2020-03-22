5240c25f727a5fea4b649c7ea3919c5ef28caa3f
doctorbebo <byronrb92@gmail.com>
2020-03-22 15:04:08 -0700

    updeted schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Turkery Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', true);
